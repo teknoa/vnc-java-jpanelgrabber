3f4632d981a8e24ac619048889f295ba2e23b09b
Matthias K <m.klapperstueck@gmail.com>
2014-12-06 23:47:43 +1100

    working prototype, that copies rendered JPanel content to VNC Client

--- src/gnu/vnc/pixels/VNCPixels.java
+++ src/gnu/vnc/pixels/VNCPixels.java
@@ -1,27 +1,27 @@
 package gnu.vnc.pixels;
 
 /**
- * <br><br><center><table border="1" width="80%"><hr>
- * <strong><a href="http://www.amherst.edu/~tliron/vncj">VNCj</a></strong>
- * <p>
- * Copyright (C) 2000-2002 by Tal Liron
- * <p>
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * <p>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * <a href="http://www.gnu.org/copyleft/lesser.html">GNU Lesser General Public License</a>
- * for more details.
- * <p>
- * You should have received a copy of the <a href="http://www.gnu.org/copyleft/lesser.html">
- * GNU Lesser General Public License</a> along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- * <hr></table></center>
- **/
+* <br><br><center><table border="1" width="80%"><hr>
+* <strong><a href="http://www.amherst.edu/~tliron/vncj">VNCj</a></strong>
+* <p>
+* Copyright (C) 2000-2002 by Tal Liron
+* <p>
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* as published by the Free Software Foundation; either version 2.1
+* of the License, or (at your option) any later version.
+* <p>
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* <a href="http://www.gnu.org/copyleft/lesser.html">GNU Lesser General Public License</a>
+* for more details.
+* <p>
+* You should have received a copy of the <a href="http://www.gnu.org/copyleft/lesser.html">
+* GNU Lesser General Public License</a> along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+* <hr></table></center>
+**/
 
 import gnu.vnc.*;
 import gnu.rfb.*;
@@ -34,20 +34,20 @@ import java.util.*;
 public class VNCPixels implements RFBServer, PixelsOwner
 {
 
-	/**
-	 * VNCEvents to forward events to
-	 */
-	private gnu.vnc.awt.VNCEvents events = null;
+ /**
+  * VNCEvents to forward events to
+  */
+ private gnu.vnc.awt.VNCEvents events = null;
 
-	public void setVNCEventsHandler(gnu.vnc.awt.VNCEvents events)
-	{
-		this.events = events;
-	}
+ public void setVNCEventsHandler(gnu.vnc.awt.VNCEvents events)
+ {
+  this.events = events;
+ }
 
-	public gnu.vnc.awt.VNCEvents getVNCEventsHandler()
-	{
-		return events;
-	}
+ public gnu.vnc.awt.VNCEvents getVNCEventsHandler()
+ {
+  return events;
+ }
 
 
 
@@ -63,17 +63,17 @@ public class VNCPixels implements RFBServer, PixelsOwner
 
 		queue = new VNCQueue( clients );
 
-		pixelArray = new int[ width * height];
+		pixelArray = new int[ width * height ];
 	}
 
-	/**
-	 * externalize queue to manually add entries
-	 * @return
-	 */
-	public VNCQueue getQueue()
-	{
-		return(queue);
-	}
+        /**
+         * externalize queue to manually add entries
+         * @return
+         */
+        public VNCQueue getQueue()
+        {
+         return(queue);
+        }
 
 	//
 	// Operations
@@ -94,10 +94,10 @@ public class VNCPixels implements RFBServer, PixelsOwner
 		clients.addClient( client );
 	}
 
-	public RFBClients getClients()
-	{
-		return(clients);
-	}
+        public RFBClients getClients()
+        {
+         return(clients);
+        }
 
 	public void removeClient( RFBClient client )
 	{
@@ -154,6 +154,7 @@ public class VNCPixels implements RFBServer, PixelsOwner
 
 	public void setEncodings( RFBClient client, int[] encodings ) throws IOException
 	{
+		client.setPreferredEncoding( rfb.EncodingRRE );
 	}
 
 	public void fixColourMapEntries( RFBClient client, int firstColour, Colour[] colourMap ) throws IOException
@@ -162,34 +163,34 @@ public class VNCPixels implements RFBServer, PixelsOwner
 
 	public void frameBufferUpdateRequest( RFBClient client, boolean incremental, int x, int y, int w, int h ) throws IOException
 	{
-		queue.takeSnapshot(this);
-		queue.addRectangle(this);
-		queue.frameBufferUpdate( client, incremental, x, y, w, h);
+            queue.takeSnapshot(this);
+            queue.addRectangle(this);
+	    queue.frameBufferUpdate( client, incremental, x, y, w, h);
 	}
 
 	public void keyEvent( RFBClient client, boolean down, int key ) throws IOException
 	{
-		//         System.err.println("DEBUG[VNCPixels] keyEvent");
-		if( events!=null)
-		{
-			events.translateKeyEvent(client, down, key);
-		}
-		else
-		{
-			updateAll();
-		}
+//         System.err.println("DEBUG[VNCPixels] keyEvent");
+        if( events!=null)
+        {
+            events.translateKeyEvent(client, down, key);
+        }
+        else
+        { 
+        	updateAll();
+        }
 	}
 
 	public void pointerEvent( RFBClient client, int buttonMask, int x, int y ) throws IOException
 	{
-		//         System.err.println("DEBUG[VNCPixels] pointerEvent");
-		if( events!=null) {
-			events.translatePointerEvent(client, buttonMask, x, y);
-		}
-		else
-		{
-			updateAll();
-		}
+//         System.err.println("DEBUG[VNCPixels] pointerEvent");
+        if( events!=null) {
+            events.translatePointerEvent(client, buttonMask, x, y);
+        }
+        else
+        {
+        	updateAll();
+        }
 	}
 
 	public void clientCutText( RFBClient client, String text ) throws IOException
@@ -226,7 +227,7 @@ public class VNCPixels implements RFBServer, PixelsOwner
 			RFBClient client = (RFBClient)e.nextElement();
 			frameBufferUpdateRequest( client, false, 0, 0, width, height );
 		}
-
+		
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
--- src/vncjdemo/VNCMouseDraw.java
+++ src/vncjdemo/VNCMouseDraw.java
@@ -7,11 +7,19 @@ import gnu.vnc.WebServer;
 import gnu.vnc.pixels.VNCPixels;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DirectColorModel;
+import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.SinglePixelPackedSampleModel;
+import java.awt.image.WritableRaster;
 import java.net.Authenticator;
 
 import javax.swing.JFrame;
@@ -21,6 +29,7 @@ import javax.swing.SwingUtilities;
 public class VNCMouseDraw extends JPanel{
 
 	boolean mouseClicked = false;
+	private boolean clearscreen;
 
 	MouseEvent lastMouseEvent;
 
@@ -36,6 +45,10 @@ public class VNCMouseDraw extends JPanel{
 
 	private static VNCMouseDraw vncMouseDraw;
 
+	private int mergedpixels[];
+
+	private int[] rawpixelarrays;
+
 	public VNCMouseDraw() {
 
 
@@ -47,36 +60,69 @@ public class VNCMouseDraw extends JPanel{
 		// TODO Auto-generated method stub
 		//		super.paintComponent(g);
 
-		if(mouseClicked) {
+//		if(mouseClicked) {
 			mouseClicked = false;
-			
-			bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-			
+			if(bi == null)
+			{	
+//				bi = createImage(getWidth(), getHeight());
+				bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+				int num = getWidth() * getHeight() * bi.getRaster().getNumBands();
+				rawpixelarrays = new int[num];
+				
+			}
+
 			Graphics big = bi.getGraphics();
-			
-			big.drawRect(lastMouseEvent.getX(), lastMouseEvent.getY(), 10,10);
-
+			Graphics2D g2d = (Graphics2D)big;
+			g2d.setBackground(new Color(255,128,1));
+			if(clearscreen)
+				g2d.clearRect(0, 0, getWidth(), getHeight());
+			clearscreen = false;
+			g2d.setColor(Color.red);
+			g2d.fillRect(lastMouseEvent.getX()-5, lastMouseEvent.getY()-5, 10,10);
 			g.drawImage(bi, 0, 0, null);
-			
-			
-			int num = getWidth() * getHeight() * bi.getRaster().getNumBands();
-			int[] array = new int[num];
-			pixels = bi.getRaster().getPixels(0, 0, getWidth(), getHeight(), array);
-			vncPixels.setPixelArray(pixels, getWidth(), getHeight());
 
-			
+			pixels = bi.getRaster().getPixels(0, 0, getWidth(), getHeight(), rawpixelarrays);
+			for(int i = 0; i < getWidth() * getHeight(); i++) {
+				int b = 0;
+				b |= pixels[i];
+				mergedpixels[i] = 0;
+				mergedpixels[i] |= pixels[3*i+2]; 
+				mergedpixels[i] |= pixels[3*i+1] << 8; 
+				mergedpixels[i] |= pixels[3*i] << 16;
+			}
+//			vncPixels.setPixelArray(mergedpixels, getWidth(), getHeight());
+
+
 			vncPixels.getQueue().takeSnapshot(vncPixels);
-			
-			
-		}
+
+
+//		}
 	}
 
+	public BufferedImage createImage( int width, int height )
+	{
+		//System.err.println( "createImage("+width+","+height+")" );
+
+		// Color model
+		DirectColorModel colorModel = (DirectColorModel) getColorModel();
+
+		// Sample model
+		SampleModel sampleModel = new SinglePixelPackedSampleModel( DataBuffer.TYPE_INT, width, height, colorModel.getMasks() );
+
+		// Raster
+		WritableRaster raster = Raster.createWritableRaster( sampleModel, null );
+
+		// Image
+		return new BufferedImage( colorModel, raster, true, null );
+	}
+
+	
 	void setupServer() {
 		MyMouseAdapter ma = new MyMouseAdapter();
 		addMouseListener(ma);
 		addMouseMotionListener(ma);
 
-		bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+		bi = createImage(getWidth(), getHeight());
 		int num = getWidth() * getHeight() * bi.getRaster().getNumBands();
 		int[] array = new int[num];
 		pixels = bi.getRaster().getPixels(0, 0, getWidth(), getHeight(), array);
@@ -91,7 +137,11 @@ public class VNCMouseDraw extends JPanel{
 		System.out.println( displayName );
 
 		vncPixels = new VNCPixels(displayName, getWidth(), getHeight());
-		vncPixels.setPixelArray(pixels, getWidth(), getHeight());
+		mergedpixels = new int[getWidth() * getHeight()];
+		for(int i = 0; i < getWidth() * getHeight(); i++)
+			mergedpixels[i] = 0x00ff8811; 
+		
+		vncPixels.setPixelArray(mergedpixels, getWidth(), getHeight());
 
 		// RFB host
 		rfbHost = new RFBHost( display, displayName, vncPixels, authenticator );
@@ -111,6 +161,7 @@ public class VNCMouseDraw extends JPanel{
 			// TODO Auto-generated method stub
 			super.mouseClicked(e);
 			mouseClicked = true;
+			clearscreen = e.getButton() == MouseEvent.BUTTON3 ? true : false;
 			lastMouseEvent = e;
 			repaint();
 		}
@@ -119,6 +170,8 @@ public class VNCMouseDraw extends JPanel{
 		public void mouseDragged(MouseEvent e) {
 			// TODO Auto-generated method stub
 			super.mouseDragged(e);
+			lastMouseEvent = e;
+			repaint();
 		}
 
 
@@ -128,7 +181,7 @@ public class VNCMouseDraw extends JPanel{
 
 	public static void main(String[] args) {
 		frame = new JFrame("vnc drawing test");
-		frame.setSize(800, 600);
+		frame.setSize(600, 600);
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		vncMouseDraw = new VNCMouseDraw();
